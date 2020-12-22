3fb8ca78db7981777d21c31a7da7cc4f421c8cdb
smita.webdev <smita.webdev09@gmail.com>
2020-12-21 19:18:54 -0500

    added app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+//Selectors
+const btnTranslate = document.querySelector("#btn-translate");
+const inputTxt = document.querySelector("#txt-input");
+const outputTxt = document.querySelector("#txt-output");
+
+//Event listeners
+btnTranslate.addEventListener("click", clickHandler);
+
+//Functions
+let serverURL = "https://api.funtranslations.com/translate/groot.json";
+
+function createURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("Some Error Occured", error);
+    alert("Oops something went wrong!");
+}
+
+function clickHandler() {
+
+    let content = inputTxt.value;
+
+    //Making call to server
+    fetch(createURL(content))
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            outputTxt.innerText = json.contents.translated;
+
+        })
+        .catch(errorHandler);
+};
